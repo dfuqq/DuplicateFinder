e304e839e4672a8147770cfee1355aca00b7d460
dfuqq <dan38449@mail.ru>
2026-02-11 04:49:06 +0500

    fix: chunked arr helper w/ explicit init

--- DuplicateFinder/Utilities/Extensions.swift
+++ DuplicateFinder/Utilities/Extensions.swift
@@ -5,10 +5,14 @@
 //  Created by da fuq on 11.02.2026.
 //
 
+import Foundation
+
 extension Array {
   func chunked(into n: Int) -> [[Element]] {
     guard n > 0, !isEmpty else { return [self] }
-    var chunks: [[Element]] = Array(repeating: [], count: n)
+    var chunks: [[Element]] = []
+    chunks.reserveCapacity(n)
+    for _ in 0..<n { chunks.append([]) }
     for (i, el) in self.enumerated() {
       chunks[i % n].append(el)
     }
