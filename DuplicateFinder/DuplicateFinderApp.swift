78252185d443d32a1ce72117e975c1e39c7f63b9
dfuqq <dan38449@mail.ru>
2026-02-11 03:51:15 +0500

    Initial Commit

---
+++ DuplicateFinder/DuplicateFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuplicateFinderApp.swift
+//  DuplicateFinder
+//
+//  Created by da fuq on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DuplicateFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
